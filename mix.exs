74e3d54006b4a963eefd29ee8f922ae97574fccc
Matt Silbernagel <silbermm@gmail.com>
2020-09-20 20:39:57 -0400

    add license

--- mix.exs
+++ mix.exs
@@ -4,6 +4,8 @@ defmodule Diceware.MixProject do
   def project do
     [
       app: :diceware,
+      description: "Generate passphrases using the Diceware method",
+      package: package(),
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
@@ -19,7 +21,17 @@ defmodule Diceware.MixProject do
 
   defp deps do
     [
+
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["3-Clause BSD License"],
+      files: ["lib", "mix.exs", "README.md", "LICENSE*"],
+      maintainers: [ "Matt Silbernagel" ],
+      links: %{:GitHub => "https://github.com/silbermm/diceware"}
     ]
   end
 end
